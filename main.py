3be2811cce4bd26fa9afb7bcee3382df8269d84d
xavans2 <xaviervanschoor.xavans2@gmail.com>
2026-05-01 22:37:40 +0200

    Add difficulty parameter to Miner initialization

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import json
 from src.api_client import APIClient
 from src.miner import Miner
@@ -7,6 +6,11 @@ with open("config/miner_config.json") as f:
     config = json.load(f)
 
 api = APIClient(config["node_url"])
-miner = Miner(api, config["miner_name"])
+
+miner = Miner(
+    api,
+    config["miner_name"],
+    difficulty=4
+)
 
 miner.start()
