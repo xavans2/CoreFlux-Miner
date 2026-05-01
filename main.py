4da4429454553a43b81a1585e23f41a7b28416dd
xavans2 <xaviervanschoor.xavans2@gmail.com>
2026-05-01 23:02:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import json
-from src.api_client import APIClient
 from src.miner import Miner
 
 with open("config/miner_config.json") as f:
     config = json.load(f)
 
-api = APIClient(config["node_url"])
-
-miner = Miner(
-    api,
-    config["miner_name"],
-    difficulty=4
-)
-
+miner = Miner(config)
 miner.start()
